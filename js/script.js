5f91b6a76a2cf792d8ce248f791cdaf15cd9c643
Marcin Kalinowski <mjk.gdansk@gmail.com>
2023-03-20 19:21:06 +0100

    feat: add ceritficates slider

--- js/script.js
+++ js/script.js
@@ -54,5 +54,122 @@ const obs = new IntersectionObserver(
     rootMargin: "-96px",
   }
 );
-obs.observe(sectionHeroEl);
+// obs.observe(sectionHeroEl);
 // obs.observe(galleryEl);
+
+/* const xeniaModal = document.querySelector(".xenia-modal");
+const primModal = document.querySelector(".prim-modal");
+const frodoModal = document.querySelector(".frodo-modal");
+const xeniaShowModal = document.querySelector(".xenia-show-modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+
+const closeModal = function () {
+  xeniaModal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+xeniaShowModal.addEventListener("click", function () {
+  xeniaModal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+});
+
+overlay.addEventListener("click", function () {
+  xeniaModal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    if (!xeniaModal.classList.contains("hidden")) {
+      xeniaModal.classList.add("hidden");
+      overlay.classList.add("hidden");
+    }
+  }
+});
+
+btnCloseModal.addEventListener("click", closeModal); */
+
+const slider = function () {
+  const slides = document.querySelectorAll(".slide");
+  const btnRight = document.querySelector(".slider-btn-right");
+  const btnLeft = document.querySelector(".slider-btn-left");
+  const maxSlide = slides.length;
+  let currSlide = 0;
+
+  const dotContainer = document.querySelector(".dots");
+
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots-dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll(".dots-dot")
+      .forEach((dot) => dot.classList.remove("dots-dot--active"));
+
+    document
+      .querySelector(`.dots-dot[data-slide="${slide}"]`)
+      .classList.add("dots-dot--active");
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  // Next slide
+  const nextSlide = function () {
+    if (currSlide === maxSlide - 1) {
+      currSlide = 0;
+    } else {
+      currSlide++;
+    }
+    goToSlide(currSlide);
+    activateDot(currSlide);
+  };
+
+  const prevSlide = function () {
+    if (currSlide === 0) {
+      currSlide = maxSlide - 1;
+    } else {
+      currSlide--;
+    }
+    goToSlide(currSlide);
+    activateDot(currSlide);
+  };
+
+  const init = function () {
+    createDots();
+    activateDot(0);
+    goToSlide(0);
+  };
+
+  init();
+
+  // Event handlers
+
+  btnRight.addEventListener("click", nextSlide);
+  btnLeft.addEventListener("click", prevSlide);
+
+  document.addEventListener("keydown", function (e) {
+    e.key === "ArrowLeft" && prevSlide();
+    e.key === "ArrowRight" && nextSlide();
+  });
+
+  dotContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots-dot")) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateDot(slide);
+    }
+  });
+};
+
+slider();
